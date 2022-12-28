5b58ede3a6b14af8d44f17dcfcd2d1d99e342dc1
Matheus Silva <98681828+Tetheugas@users.noreply.github.com>
2022-12-28 09:33:17 -0300

    Create cafe.js

---
+++ cafe.js
@@ -0,0 +1,62 @@
+let coffeJson = [
+    {
+        id: 1,
+        name: 'Café Expresso',
+        img: 'img/espresso-coffee-glass.jpg',
+        price: [3.00, 5.00, 8.00],
+        sizes: [
+          '150ml',
+          '350ml',
+          '500ml'
+        ],
+        description: 'Café feito na máquina de café, expresso.'
+      },
+      {
+        id: 2,
+        name: 'Capuccino',
+        img: 'img/capuccino2.jpg',
+        price: [6.00, 9.00, 12.00],
+        sizes: [
+          '250ml',
+          '400ml',
+          '580ml'
+        ],
+        description: 'Consiste em um terço de café expresso,<br> um terço de leite vaporizado e um terço de espuma de leite vaporizado.'
+      },
+      {
+        id: 3,
+        name: 'Café latte',
+        img: 'img/cafe.jpg',
+        price: [4.00, 6.00, 9.00],
+        sizes: [
+          '250ml',
+          '400ml',
+          '580ml'
+        ],
+        description: 'Café de origem italiana feita com café expresso e leite vaporizado<br> com uma quantidade generosa de espuma de leite no topo.'
+      },
+      {
+        id: 4,
+        name: 'Bolo de Milho',
+        img: 'img/bolo1.jpeg',
+        price: [3.00, 5.00, 6.00],
+        sizes: [
+          '1 fatias',
+          '2 fatias',
+          '3 fatias'
+        ],
+        description: 'Bolo de Milho verde, feito no mesmo dia.'
+      },
+      {
+        id: 5,
+        name: 'Bolo Red Valvet',
+        img: 'img/bolo-red-velvet-01.jpg',
+        price: [8.00, 14.00, 20.00],
+        sizes: [
+          '1 fatias',
+          '2 fatias',
+          '3 fatias'
+        ],
+        description: 'É um bolo de camadas, coberto com butter cream.'
+      }
+];
